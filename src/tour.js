8d838630b317c33a18035fee89ec97fbcad7c677
Morton Kuo <menghsinkuo@gmail.com>
2022-09-30 17:10:42 +0800

    feat: introduction for sidebar header

--- src/tour.js
+++ src/tour.js
@@ -1,13 +1,30 @@
 const steps = [
   {
+    element: '#all',
+    title: 'All Colleagues',
+    content: 'Click to show all colleagues',
+  },
+  {
+    element: '#star',
+    title: 'Starred Colleagues',
+    content: 'Click to show starred colleagues',
+  },
+  {
+    element: '#group',
+    title: 'Group Channels',
+    content: 'Click to show group channels you have joined',
+  },
+  {
     element: '#groupAddParticipants',
     title: 'Manage Group Channel',
     content: 'Create group channel, add or delete group member',
+    placement: 'top',
   },
   {
     element: '#profile',
     title: 'Edit Profile',
     content: 'Edit password or profile picture',
+    placement: 'top',
   },
 ];
 
@@ -18,9 +35,10 @@ if (role === '-1')
     element: '#adminButton',
     title: 'Admin Panel',
     content: 'Edit Firm Logo, create or delete user account',
+    placement: 'top',
   });
 
-// Instance the tour
+// eslint-disable-next-line no-undef
 const tour = new Tour({
   steps,
 });
